5db8b54300689b2c8c43ccca5eafcdff49ec5a02
piyush-jain <piyushjain1sa@gmail.com>
2016-09-01 04:45:11 +0530

    Editor Switching done

--- app.py
+++ app.py
@@ -18,4 +18,4 @@ def dashboard():
 		return render_template('tables.html')
 
 if __name__ == '__main__':
-	app.run(host='0.0.0.0',port=5000,debug=True)
+	app.run(host='0.0.0.0',port=5001,debug=True)
--- static/js/main.js
+++ static/js/main.js
@@ -1,3 +1,21 @@
+editor_codes = [ "" , "" , "" , "" ];
+current_code = "";
+current_question = 1;
+
+
+// var editor = ace.edit("editor");
+// editor.setTheme("ace/theme/terminal");
+// editor.session.setMode("ace/mode/text");
+var editor = ace.edit('editor');
+editor.session.setMode("ace/mode/batchfile");
+editor.setTheme("ace/theme/monokai");
+editor.getSession().on( "change", function () {
+	current_code = editor.getSession().getValue();
+	// console.log(input.val());
+});
+
+
+
 $('#compile').click(function() {
 	var textarea_value = $("#inp").val();
 	if(textarea_value=='') {
@@ -12,16 +30,20 @@ $('#clear').click(function() {
 	editor.setValue(""); 
 });
 
-var editor = ace.edit("editor");
-editor.setTheme("ace/theme/terminal");
-editor.session.setMode("ace/mode/text");
-var editor = ace.edit('editor');
-editor.session.setMode("ace/mode/batchfile");
-editor.setTheme("ace/theme/monokai");
 
-var input = $('#inp');
 
-editor.getSession().on( "change", function () {
-	input.val(editor.getSession().getValue());
-	//console.log(input.val());
+$('.question_select').click(function(){
+	$('.question_select').removeClass('active');
+	$(this).addClass('active');
+	editor_codes[current_question-1] = current_code;
+	current_question = ($('a',this).attr('id'))[1];
+	editor.setValue(editor_codes[current_question-1]);	
 });
+
+
+
+// function getset(pactive, pnew)
+// {
+// 	pactive=input
+// 	input=pnew
+// }
